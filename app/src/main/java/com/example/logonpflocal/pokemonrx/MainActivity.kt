d3300d353ff25c0284a3a2d7064ec9e6f1ecd963
Heider Lopes <heidertreinamentos@gmail.com>
2019-02-21 22:41:28 -0300

    Inclusao da lista

--- app/src/main/java/com/example/logonpflocal/pokemonrx/MainActivity.kt
+++ app/src/main/java/com/example/logonpflocal/pokemonrx/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.logonpflocal.pokemonrx
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.example.logonpflocal.pokemonrx.api.PokemonAPI
 import com.example.logonpflocal.pokemonrx.api.PokemonService
+import com.example.logonpflocal.pokemonrx.model.Pokemon
+import kotlinx.android.synthetic.main.activity_main.*
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: PokemonAdapter
+
+    val pokemons = ArrayList<Pokemon>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +24,19 @@ class MainActivity : AppCompatActivity() {
 
         val api = PokemonService()
 
+        adapter = PokemonAdapter(this, pokemons, {})
+        rvPokemons.adapter = adapter
+        //rvPokemons.layoutManager = LinearLayoutManager(this)
+        rvPokemons.layoutManager = GridLayoutManager(this, 3)
+
         api.loadPokemons()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        {Log.i("POKEMON", "${it.name} - ${it.sprites.frontDefault}")},
+                        {pokemons.add(it)},
                         {e -> e.printStackTrace()},
-                        {Log.i("POKEMON", "Todos pokemons emitidos")}
+                        {adapter.notifyDataSetChanged()
+                        }
                 )
     }
 }
